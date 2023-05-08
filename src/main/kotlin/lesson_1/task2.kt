c7260f1f479b91d2194e3ea0495cccf4b5b633cb
VladiSlave-Alexandrov <yippee-ki-yay2468@mail.ru>
2023-05-08 19:14:52 +0700

    KS-1-2 Solution of task 2 in lesson 1

---
+++ src/main/kotlin/lesson_1/task2.kt
@@ -0,0 +1,19 @@
+package lesson_1
+
+fun main() {
+
+    var numberStaff: Int = 2000
+
+    val orderQuantity: Int = 75
+
+    val thanksForBuying: String = "Спасибо за покупку, приходите еще"
+
+    println(orderQuantity)
+    println(thanksForBuying)
+    //println(numberStaff)
+
+    numberStaff = 1999
+
+    println(numberStaff)
+
+}
