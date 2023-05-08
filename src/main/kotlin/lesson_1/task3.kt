e31aea242d5649c8d7869f6e3c38383025c6cac5
VladiSlave-Alexandrov <yippee-ki-yay2468@mail.ru>
2023-05-08 18:25:50 +0700

    KS-1-3 Solution of task 1 in lesson 3

---
+++ src/main/kotlin/lesson_1/task3.kt
@@ -0,0 +1,19 @@
+package lesson_1
+
+fun main(){
+
+    val year = "1961"
+    var hour = "09"
+    var minute = "07"
+
+    println(year)
+    println(hour)
+    println(minute)
+    println()
+
+    hour = "10"
+    minute = "55"
+
+    println(year)
+    println("$hour:$minute")
+}
