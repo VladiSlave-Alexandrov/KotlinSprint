6fe7032247344b3daa272e4f2c0f9a0303643ca5
VladiSlave-Alexandrov <yippee-ki-yay2468@mail.ru>
2023-05-09 22:40:09 +0700

    KS-2-4 Solution of task 4 in lesson 2

---
+++ src/main/kotlin/lesson2/task4.kt
@@ -0,0 +1,12 @@
+package lesson2
+
+fun main(){
+    val crystalOre = 7
+    val ironOre = 11
+
+    val buffCrystalOre = crystalOre / 5
+    val buffIronOre = ironOre / 5
+
+    println("Бонусной кристалической руды: $buffCrystalOre")
+    println("Бонусной железной руды: $buffIronOre")
+}
