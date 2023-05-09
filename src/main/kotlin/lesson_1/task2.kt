bc518a68969c554253a1f29e4551588d1b618297
VladiSlave-Alexandrov <yippee-ki-yay2468@mail.ru>
2023-05-09 19:39:11 +0700

    KS-1-5 Solution of task 5 in lesson 1

--- src/main/kotlin/lesson_1/task2.kt
+++ src/main/kotlin/lesson_1/task2.kt
@@ -3,9 +3,7 @@ package lesson_1
 fun main() {
 
     var numberStaff: Int = 2000
-
     val orderQuantity: Int = 75
-
     val thanksForBuying: String = "Спасибо за покупку, приходите еще"
 
     println(orderQuantity)
@@ -13,7 +11,6 @@ fun main() {
     //println(numberStaff)
 
     numberStaff = 1999
-
     println(numberStaff)
 
 }
