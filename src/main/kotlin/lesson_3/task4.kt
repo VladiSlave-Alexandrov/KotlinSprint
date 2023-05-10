5b151c8681d0913f15860b4a43fd2c2f638c6d31
VladiSlave-Alexandrov <yippee-ki-yay2468@mail.ru>
2023-05-10 19:04:00 +0700

    KS-3-4 Solution of task 4 in lesson 3

---
+++ src/main/kotlin/lesson_3/task4.kt
@@ -0,0 +1,14 @@
+package lesson_3
+
+fun main(){
+
+    var startMove = "E2"
+    var finalMove = "E4"
+    var numberMove = 1
+    println("$numberMove[$startMove -> $finalMove]")
+
+    startMove = "D2"
+    finalMove = "D3"
+    numberMove = 2
+    println("$numberMove[$startMove -> $finalMove]")
+}
