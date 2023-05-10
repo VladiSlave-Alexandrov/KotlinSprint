6bfaf8486563ba8bd9ba304b2150a4d8b36a5e28
VladiSlave-Alexandrov <yippee-ki-yay2468@mail.ru>
2023-05-10 18:14:46 +0700

    KS-3-1 Solution of task 1 in lesson 3

---
+++ src/main/kotlin/lesson_3/task1.kt
@@ -0,0 +1,12 @@
+package lesson_3
+
+fun main(){
+
+    val userName = "Владислав"
+    var greeting = "Доброе утро $userName"
+
+    println(greeting)
+    greeting = "Добрый вечер $userName"
+    println(greeting)
+
+}
