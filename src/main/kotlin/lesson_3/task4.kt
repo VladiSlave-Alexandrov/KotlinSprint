3017f05d5cf350b281d63906ef8313b05ff4e629
VladiSlave-Alexandrov <yippee-ki-yay2468@mail.ru>
2023-05-11 20:32:55 +0700

    KS-3-4 Solution of task 4 in lesson 3

--- src/main/kotlin/lesson_3/task4.kt
+++ src/main/kotlin/lesson_3/task4.kt
@@ -5,10 +5,10 @@ fun main(){
     var startMove = "E2"
     var finalMove = "E4"
     var numberMove = 1
-    println("$numberMove[$startMove -> $finalMove]")
+    println("[$startMove-$finalMove;$numberMove]")
 
     startMove = "D2"
     finalMove = "D3"
     numberMove = 2
-    println("$numberMove[$startMove -> $finalMove]")
+    println("[$startMove-$finalMove;$numberMove]")
 }
