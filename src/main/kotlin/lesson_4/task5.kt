eb76fb18d7137859cb71421dfe40919dacedefc9
VladiSlave-Alexandrov <yippee-ki-yay2468@mail.ru>
2023-05-14 13:23:20 +0700

    KS-4-5 Solution of task 5 in lesson 4

---
+++ src/main/kotlin/lesson_4/task5.kt
@@ -0,0 +1,18 @@
+package lesson_4
+
+fun main(){
+    print("Есть ли повреждения корпуса(true - незначительные,false - отсутствуют): ")
+    val damages: Boolean? = readlnOrNull()?.toBoolean()
+
+    print("Введите численность экипажа: ")
+    val crewNumber: Int? = readlnOrNull()?.toInt()
+
+    print("Введите текущее количество ящиков провизии: ")
+    val provisions: Int? = readlnOrNull()?.toInt()
+
+    print("Погодные условия(true - благоприятные, false - неблагоприятные): ")
+    val weather: Boolean? = readlnOrNull()?.toBoolean()
+
+    val shippingOption = ((damages == false)&&(crewNumber!! in 55..69)&&(provisions!! > 50))||((damages == true)&&(crewNumber!! == 70)&&(provisions!! > 50)&&(weather == true))
+    println(shippingOption)
+}
