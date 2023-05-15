9445c00ad77742a8ae9a30119db722efc839672c
VladiSlave-Alexandrov <yippee-ki-yay2468@mail.ru>
2023-05-15 19:49:21 +0700

    KS-4-4 Solution of task 4 in lesson 4

--- src/main/kotlin/lesson_4/task4.kt
+++ src/main/kotlin/lesson_4/task4.kt
@@ -2,11 +2,15 @@ package lesson_4
 
 fun main(){
     val trainingDay = 5
-    var exercise = trainingDay % 2
+    var exercise = trainingDay % 2 == 1
+    val hands = exercise
+    val abs = exercise
+    val legs = !exercise
+    val back = !exercise
 
     println("""
-        Упражнения для рук:    ${exercise == 1}      
-        Упражнения для ног:    ${exercise == 0}
-        Упражнения для спины:  ${exercise == 0}
-        Упражнения для пресса: ${exercise == 1}""")
+        Упражнения для рук:    $hands      
+        Упражнения для ног:    $legs
+        Упражнения для спины:  $back
+        Упражнения для пресса: $abs""")
 }
