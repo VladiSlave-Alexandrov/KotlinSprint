46a791c7bc33d8e889c8629d17757c684977a06f
VladiSlave-Alexandrov <yippee-ki-yay2468@mail.ru>
2023-05-15 19:16:58 +0700

    KS-5-4 Solution of task 3 in lesson 4

--- src/main/kotlin/lesson_4/task3.kt
+++ src/main/kotlin/lesson_4/task3.kt
@@ -1,11 +1,11 @@
 package lesson_4
 
 fun main(){
-    val weather = "солнечная"
-    val awning = "раскрыт"
+    val weather = true
+    val isTentOpen = true
     val humidity = 20
-    val season = "зима"
+    val season = false
 
-    val conditions = (weather == "солнечная") && (awning == "раскрыт") && (humidity == 20) && (season !="зима")
+    val conditions = weather && isTentOpen && (humidity == 20) && season
     println("Благоприятные ли условия сейчас для роста бобовых? $conditions")
 }
