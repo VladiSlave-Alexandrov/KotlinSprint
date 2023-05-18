1fbcd8ef4151dce112034fb2c5bf1bd8dbc5c0e1
VladiSlave-Alexandrov <yippee-ki-yay2468@mail.ru>
2023-05-18 18:30:19 +0700

    KS-4-5 Solution of task 5 in lesson 4

--- src/main/kotlin/lesson_4/task5.kt
+++ src/main/kotlin/lesson_4/task5.kt
@@ -13,11 +13,13 @@ fun main() {
     print("Погодные условия(true - благоприятные, false - неблагоприятные): ")
     val weather: Boolean? = readlnOrNull()?.toBoolean()
 
-    val shippingOption =
-        ((damages == false) && (crewNumber in MIN_CREW until MAX_CREW) && (provisions > 50)) || ((damages == true) && (crewNumber == MAX_CREW) && (provisions > ALLOWABLE_AMOUNT_OF_PROVISIONS) && (weather == true))
+    val shippingOption = ((damages == false) && (crewNumber in MIN_CREW until MAX_CREW) &&
+            (provisions > MIN_PROVISIONS)) || ((damages == true) && (crewNumber == MAX_CREW) &&
+            (provisions > ALLOWABLE_AMOUNT_OF_PROVISIONS) && (weather == true))
     println(shippingOption)
 }
 
 const val ALLOWABLE_AMOUNT_OF_PROVISIONS = 50
 const val MIN_CREW = 55
 const val MAX_CREW = 70
+const val MIN_PROVISIONS = 50
