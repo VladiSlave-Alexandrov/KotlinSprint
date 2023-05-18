b1e3edc58c83de9c9d24ad667390126d2a53e36d
VladiSlave-Alexandrov <yippee-ki-yay2468@mail.ru>
2023-05-18 21:05:39 +0700

    KS-4-2 Solution of task 2 in lesson 7

--- src/main/kotlin/lesson_4/task2.kt
+++ src/main/kotlin/lesson_4/task2.kt
@@ -3,9 +3,13 @@ package lesson_4
 fun main() {
     var weight = 20
     var vol = 80
-    println("Average для груза с весом $weight кг и объемом $vol л: ${(weight >= 35) && (weight <= 100) && (vol < 100)}")
+    println("Average для груза с весом $weight кг и объемом $vol л: ${(weight >= MIN_WEIGHT) && (weight <= MAX_WEIGHT) && (vol < MAX_VOL)}")
 
     weight = 50
     vol = 100
-    println("Average для груза с весом $weight кг и объемом $vol л: ${(weight >= 35) && (weight <= 100) && (vol < 100)}")
+    println("Average для груза с весом $weight кг и объемом $vol л: ${(weight >= MIN_WEIGHT) && (weight <= MAX_WEIGHT) && (vol < MAX_VOL)}")
 }
+
+const val MIN_WEIGHT = 35
+const val MAX_WEIGHT = 100
+const val MAX_VOL = 100
