0bf54952036039c09b12da80c50e4e0c05e135fc
VladiSlave-Alexandrov <yippee-ki-yay2468@mail.ru>
2023-05-20 17:05:08 +0700

    KS-7-3 Solution of task 3 in lesson 7

--- src/main/kotlin/lesson_7/task3.kt
+++ src/main/kotlin/lesson_7/task3.kt
@@ -4,8 +4,7 @@ fun main() {
     print("Введите число: ")
     val finiteNumber = readln().toInt()
 
-    for (i in 0..finiteNumber step 1) {
-        if (i % 2 == 0) print("$i, ")
-        else continue
+    for (i in 0..finiteNumber step 2) {
+        print("$i, ")
     }
 }
