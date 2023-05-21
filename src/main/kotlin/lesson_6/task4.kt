a2719570085c5556be7315fda632d095360c4423
VladiSlave-Alexandrov <yippee-ki-yay2468@mail.ru>
2023-05-21 14:29:50 +0700

    KS-6-4 Solution of task 4 in lesson 6

--- src/main/kotlin/lesson_6/task4.kt
+++ src/main/kotlin/lesson_6/task4.kt
@@ -5,19 +5,18 @@ fun main() {
 
     var consoleNumber: Int
     var counter = NUMBER_OF_ATTEMPTS
+    var flag = true
     while (counter > 0) {
         print("Угадайте число от 1 до 9, включительно: ")
         consoleNumber = readln().toInt()
         counter--
         if (consoleNumber == mysteryNumber) {
             println("Это была великолепная игра!")
+            flag = false
             break
-        } else if (counter != 0) {
-            println("Неверно, оставшееся количество попыток($counter)")
-        } else {
-            println("Было загадано число $mysteryNumber")
-        }
+        } else println("Неверно, оставшееся количество попыток($counter)")
     }
+    if (flag) println("Было загадано число $mysteryNumber")
 }
 
 const val NUMBER_OF_ATTEMPTS = 5
