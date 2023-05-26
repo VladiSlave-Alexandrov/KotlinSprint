5a747752921d6e4abdad01c4c0b798bf106dc3e7
VladiSlave-Alexandrov <yippee-ki-yay2468@mail.ru>
2023-05-26 13:40:58 +0700

    KS-9-5 Solution of task 5 in lesson 9

--- src/main/kotlin/lesson_9/task5.kt
+++ src/main/kotlin/lesson_9/task5.kt
@@ -5,6 +5,11 @@ fun main() {
     val listIngredient = MutableList(5) { readln() }
 
     val sortedListIngredient = listIngredient.distinct().sorted().toMutableList()
+
     sortedListIngredient[0] = sortedListIngredient[0].replaceFirstChar { it.uppercase() }
-    println("${sortedListIngredient.joinToString()}.")
+
+    sortedListIngredient[(sortedListIngredient.indexOf(sortedListIngredient.last()))] =
+        sortedListIngredient.last() + "."
+
+    println(sortedListIngredient.joinToString())
 }
