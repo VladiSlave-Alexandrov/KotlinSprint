cab0e4af44f3fd7df5c61c2360954dc01c559ad2
VladiSlave-Alexandrov <yippee-ki-yay2468@mail.ru>
2023-07-03 20:37:51 +0700

    KS-13-2 Solution of task 2 in lesson 13

--- src/main/kotlin/lesson_13/task2.kt
+++ src/main/kotlin/lesson_13/task2.kt
@@ -3,13 +3,8 @@ package lesson_13
 class Contact(
     var name: String,
     var contactNumber: String,
-    _company: String?
+    var company: String? = null ?: " [не указано]"
 ) {
-    var company: String?
-
-    init {
-        company = _company ?: " [не указано]"
-    }
 
     fun outputDataContact() {
         println("Имя: $name \nНомер: $contactNumber \nКомпания: $company")
