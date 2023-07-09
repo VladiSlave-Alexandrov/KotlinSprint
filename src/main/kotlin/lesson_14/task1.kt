78299e5631a596e26f45f175b836687a68798e2f
VladiSlave-Alexandrov <yippee-ki-yay2468@mail.ru>
2023-07-09 20:49:48 +0700

    KS-14-1 Solution of task 1 in lesson 14

--- src/main/kotlin/lesson_14/task1.kt
+++ src/main/kotlin/lesson_14/task1.kt
@@ -1,6 +1,6 @@
 package lesson_14
 
-class Ship(
+open class Ship(
     val name: String,
     val speed: Int = 30,
     val payload: Int = 1000,
@@ -14,8 +14,31 @@ class Ship(
     }
 }
 
+class CargoShip(
+    name: String,
+    speed: Int = 25,
+    payload: Int = 2000,
+) : Ship(name, speed, payload) {}
+
+class Icebreaker(
+    name: String,
+    speed: Int = 28,
+    payload: Int = 900,
+) : Ship(name, speed, payload) {
+    fun breakIce() {
+        println("Корабль преодолевает лёд")
+    }
+}
+
 fun main() {
     val ship1 = Ship("Ласточка")
     ship1.move()
     ship1.freight()
+
+    val ship2 = CargoShip("Победа")
+    ship2.speed
+
+    val ship3 = Icebreaker("Снежинка")
+    ship3.payload
+    ship3.breakIce()
 }
