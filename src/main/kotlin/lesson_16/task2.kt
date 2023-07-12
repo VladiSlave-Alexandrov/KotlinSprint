513fa454eaf605d45a405bfcdcbe2e2ac7034cb1
VladiSlave-Alexandrov <yippee-ki-yay2468@mail.ru>
2023-07-12 12:22:05 +0700

    KS-16-2 Solution of task 2 in lesson 16

--- src/main/kotlin/lesson_16/task2.kt
+++ src/main/kotlin/lesson_16/task2.kt
@@ -1,10 +1,16 @@
 package lesson_16
 
+import kotlin.math.pow
+
 class Circle(private val radius: Double) {
-    private val pi = 3.14
+
     fun circleLength(): Double = 2 * pi * radius
 
-    fun circleArea(): Double = pi * radius * radius
+    fun circleArea(): Double = pi * radius.pow(2)
+
+    companion object {
+        private const val pi = 3.14
+    }
 }
 
 fun main() {
